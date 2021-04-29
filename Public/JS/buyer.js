6e29d161c3967d87459feec531ecdd2dd8846eec
Aviral <blr.aviral@gmail.com>
2021-04-29 14:46:33 +0530

    Product pages complete

---
+++ Public/JS/buyer.js
@@ -0,0 +1,85 @@
+$(function(){
+      $('input[type="number"]').niceNumber();
+});
+
+$(document).ready(function() {
+      const card = $('.card');
+      const info_btn = $('.info');
+
+      //INFO DISPLAY
+      info_btn.on('click',  function(e) {
+            e.preventDefault();
+            console.log('clicked')
+            $(this).parent().children('.description').removeClass('description-hidden');
+            
+            $('.cross').on('click', function(e) {
+                  e.preventDefault();
+                  $(this).parent().addClass('description-hidden');
+            })
+      })
+
+      //SECTION CHANGE
+      const fruits = $('.fruits');
+      const meat  = $('.meat');
+      const dairy = $('.dairy');
+      const snacks = $('.snacks');
+      const drinks = $('.drinks');
+
+      const fruits_btn = $('#fruits-btn'); 
+      const meat_btn = $('#meat-btn'); 
+      const dairy_btn = $('#dairy-btn'); 
+      const snacks_btn = $('#snacks-btn'); 
+      const drinks_btn = $('#drinks-btn'); 
+
+      fruits.show();
+      meat.hide();
+      dairy.hide();
+      snacks.hide();
+      drinks.hide();
+
+      fruits_btn.on('click', function(e) {
+            e.preventDefault();
+            meat.hide();
+            dairy.hide();
+            snacks.hide();
+            drinks.hide();
+            fruits.show();
+      })
+
+      meat_btn.on('click', function(e) {
+            e.preventDefault();
+            fruits.hide();
+            dairy.hide();
+            snacks.hide();
+            drinks.hide();
+            meat.show();
+      })
+
+      dairy_btn.on('click', function(e) {
+            e.preventDefault();
+            meat.hide();
+            fruits.hide();
+            snacks.hide();
+            drinks.hide();
+            dairy.show();
+      })
+
+      snacks_btn.on('click', function(e) {
+            e.preventDefault();
+            meat.hide();
+            dairy.hide();
+            fruits.hide();
+            drinks.hide();
+            snacks.show();
+      })
+
+      drinks_btn.on('click', function(e) {
+            e.preventDefault();
+            meat.hide();
+            dairy.hide();
+            snacks.hide();
+            fruits.hide();
+            drinks.show();
+      })
+
+});
