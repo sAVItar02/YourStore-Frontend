0a44cdff3b699893d4d96dc1f407095f914cfc00
Aviral <blr.aviral@gmail.com>
2021-04-30 04:43:37 +0530

    location api config partial

--- Public/JS/home.js
+++ Public/JS/home.js
@@ -1,4 +1,6 @@
 $(document).ready( function() {
+    //SIDEBAR
+
     const location_btn = $('#location-btn');
     const location_sidebar = $('.location-sidebar');
     const cross = $('.cross');
@@ -20,4 +22,39 @@ $(document).ready( function() {
             overlay.hide();
         })
     });
-});
+
+    //GEOCODE
+    
+    function geoCode(address){
+        const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address) +'.json?access_token=pk.eyJ1Ijoic2F2aXRhcjAyIiwiYSI6ImNrOGM1ZXo5OTAyY3Yzbm9jdHJ1bTVyajcifQ.15iE2VXcOcKOI1w6_tU2UQ&limit=1';
+        fetch(url)
+        .then((response) => response.json())
+        .then( (data) => {
+            console.log(data);
+            const longitude = data.features[0].center[0];
+            const latitude = data.features[0].center[1];
+            const place_name = data.features[0].place_name;
+            
+            let location_div = `
+                <div class="location-result"> <p> ${place_name} </p> </div>
+            `
+            $('.location-buffer').empty();
+            $('.location-buffer').append(location_div);
+
+            $('.location-result').on('click', function(e) {
+                e.preventDefault();
+                overlay.hide();
+                $(this).parents('.location-sidebar').addClass('closed');
+
+                $(this).parents('body').children('.navbar').children('#location-btn').children('.nav-link').children('.link-text').text(place_name);
+            })
+        })
+    }
+
+    $('.search-location').on('click', function() {
+        let address = $('#location').val();
+        let data = geoCode(address);
+    })
+
+    
+}); 
