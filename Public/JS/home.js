282d5b5ba062ca20d3fefd0b215807da4ce08277
Aviral <blr.aviral@gmail.com>
2021-05-01 17:53:50 +0530

    current-location, shops near me

--- Public/JS/home.js
+++ Public/JS/home.js
@@ -13,6 +13,10 @@ $(document).ready( function() {
         overlay.show();
         $('body').addClass('overlay-open');
 
+        $('.location-buffer').empty();
+        $('#location').val('');
+        $('.gps-location-buffer').empty();
+
         overlay.on('click', function() {
             $(this).parent().children('.location-sidebar').addClass('closed');
             overlay.hide();
@@ -28,38 +32,92 @@ $(document).ready( function() {
 
     //----------------GEOCODE---------------------
     
-    function geoCode(address){
-        const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address) +'.json?access_token=pk.eyJ1Ijoic2F2aXRhcjAyIiwiYSI6ImNrOGM1ZXo5OTAyY3Yzbm9jdHJ1bTVyajcifQ.15iE2VXcOcKOI1w6_tU2UQ&limit=1';
+    function geoCode(address) {
+        const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address) +'.json?access_token=pk.eyJ1Ijoic2F2aXRhcjAyIiwiYSI6ImNrOGM1ZXo5OTAyY3Yzbm9jdHJ1bTVyajcifQ.15iE2VXcOcKOI1w6_tU2UQ&limit=5';
         fetch(url)
         .then((response) => response.json())
         .then( (data) => {
             console.log(data);
-            const longitude = data.features[0].center[0];
-            const latitude = data.features[0].center[1];
-            const place_name = data.features[0].place_name;
-            
-            let location_div = `
-                <div class="location-result"> <p> ${place_name} </p> </div>
-            `
             $('.location-buffer').empty();
-            $('.location-buffer').append(location_div);
+            $('body').addClass('overlay-open');
+            
+            if(data.features.length == 0) {
+                let location_div = `
+                <div class="location-result-error"> <p> Please enter a valid location </p> </div>
+                `
+                $('.location-buffer').append(location_div);
+            }
+
+            for(let i=0; i<data.features.length; i++){
+              const longitude = data.features[i].center[0];
+              const latitude = data.features[i].center[1];
+              const place_name = data.features[i].place_name;
+              
+              let location_div = `
+                  <div class="location-result"> <p> ${place_name} </p> </div>
+              `
+              $('.location-buffer').append(location_div);
+            }
 
             $('.location-result').on('click', function(e) {
                 e.preventDefault();
                 overlay.hide();
                 $(this).parents('.location-sidebar').addClass('closed');
 
-                $(this).parents('body').children('.navbar').children('#location-btn').children('.nav-link').children('.link-text').text(place_name);
+                $(this).parents('body').children('.navbar').children('#location-btn').children('.nav-link').children('.link-text').text($(this).text());
+                $('body').removeClass('overlay-open')
             })
 
-            $('body').removeClass('overlay-open');
+            // $('body').removeClass('overlay-open');
         })
     }
 
     $('.search-location').on('click', function() {
+        $('body').addClass('overlay-open');
         let address = $('#location').val();
         let data = geoCode(address);
+        $('body').removeClass('overlay-open');
     })
+
+    //----------------REVERSE GEOCODE--------------
+
+    
+    $('.current-location').on('click', function(e) {
+        e.preventDefault();
+        navigator.geolocation.getCurrentPosition(successfulLookup);
+    })
+    
+    const successfulLookup = (position) => {
+        const {longitude, latitude} = position.coords;
+        const reverseUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(longitude) +','+ encodeURIComponent(latitude) +'.json?access_token=pk.eyJ1Ijoic2F2aXRhcjAyIiwiYSI6ImNrOGM1ZXo5OTAyY3Yzbm9jdHJ1bTVyajcifQ.15iE2VXcOcKOI1w6_tU2UQ';
+
+        fetch(reverseUrl)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            $('.gps-location-buffer').empty();
+            for(let i=0; i<data.features.length; i++) {
+                const longitude = data.features[i].center[0];
+                const latitude = data.features[i].center[1];
+                const place_name = data.features[i].place_name;
+                
+                let location_div = `
+                    <div class="location-result"> <p> ${place_name} </p> </div>
+                `
+                $('.gps-location-buffer').append(location_div);
+            }
+
+            $('.location-result').on('click', function(e) {
+                e.preventDefault();
+                overlay.hide();
+                $(this).parents('.location-sidebar').addClass('closed');
+
+                $(this).parents('body').children('.navbar').children('#location-btn').children('.nav-link').children('.link-text').text($(this).text());
+                $('body').removeClass('overlay-open')
+            })
+        })
+    }
+
 
     //----------------SLICK--------------------
     
